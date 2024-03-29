6a52dde8cb7e2211ca7edc7399b659f4017af14c
joanna barros <joanna.barros@eaportal.org>
2024-03-28 22:40:23 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -31,5 +31,13 @@ botoesGorjeta.forEach(botao => {
 })
 
 function receberPorcentagem(evento) {
-    console.log(evento.targert.value)
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.targert.value) {
+           botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.targert.value) / 100
 }
